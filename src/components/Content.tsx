426221684117777054e45dad518f1ed1f2ab809e
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-03-22 16:53:48 -0300

    fix: refactor App component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps, GenreResponseProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  movies: MovieProps[];
+  title: string;
 }
+
+export const Content: React.FC<ContentProps> = ({ movies, title }) => {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
